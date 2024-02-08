61cf5a3f73a5b05329df2132b1a8909548ec349a
Ganesh Nawghare <ganesh7003nawghare@gmail.com>
2024-02-08 20:27:40 +0530

    removed unwanted code in setting page

--- src/views/admin/settings/settings.tsx
+++ src/views/admin/settings/settings.tsx
@@ -1,12 +1,7 @@
-import Navbar from "components/navbar";
-import React, { useState } from "react";
-
 function Settings() {
 
   return (
     <div>
-      {/* <Navbar flag={false} brandText="Settings" />
-      <div>vehicleType</div> */}
     </div>
   );
 }
