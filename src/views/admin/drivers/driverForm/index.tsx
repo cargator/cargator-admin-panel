787ed842cda8b747a9a19fd53c809dc60139bf27
Deepak Asati <asatideepak601@gmail.com>
2024-05-22 19:37:23 +0530

    none Option added to select vehicle successfully

--- src/views/admin/drivers/driverForm/index.tsx
+++ src/views/admin/drivers/driverForm/index.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Card from "../../../../components/card";
 import Select from "react-select";
 import { Button } from "@chakra-ui/react";
@@ -63,7 +63,7 @@ const Logger = (props: any): JSX.Element => {
         formik.values.vehicleName = "none";
         setVehicleName("None");
         setVehicleType("None");
-        
+
       } else {
         allAvailableVehicles.map((data: any) => {
           if (data.vehicleNumber === formik.values.vehicleNumber) {
@@ -125,12 +125,7 @@ const DriverForm = () => {
     documents: [],
   });
   const [allAvailableVehicles, setAllAvailableVehicles] = useState([]);
-  const [options, setOptions] = useState([
-    {
-      value: "",
-      label: "",
-    },
-  ]);
+  const [options, setOptions] = useState([]);
   const [imageFile, setImageFile] = useState(null);
   const anchorImageRef = useRef(null);
 
@@ -256,9 +251,11 @@ const DriverForm = () => {
     try {
       console.log("hhhooooooooooooooooooooooooooooooo")
       const res = await getAvailableVehiclesApi();
+      console.log("res", res.data)
       if (!res) {
         errorToast("Vehicles not available");
       }
+      debugger
       setOptions(
         res.data.map((option: any) => {
           return {
@@ -272,6 +269,10 @@ const DriverForm = () => {
       errorToast(error.response.data.message);
     }
   };
+
+  useEffect(() => {
+    console.log({ options })
+  }, [options])
 
   async function getS3SignUrl(key: string, contentType: string, type: string) {
     const headers = { "Content-Type": "application/json" };
@@ -307,7 +308,7 @@ const DriverForm = () => {
             console.log("image key to upload :>> ", key);
             const contentType = "image/*";
             const type = "put";
-            if(key !== undefined){
+            if (key !== undefined) {
               const data: any = await getS3SignUrl(key, contentType, type);
               if (data.url) {
                 res = await pushProfilePhotoToS3(
@@ -316,7 +317,7 @@ const DriverForm = () => {
                 );
               }
             }
-           
+
             if (initialProfileImage) {
               const response = deleteObjectFromS3Api({
                 key: initialProfileImage?.key,
@@ -334,7 +335,7 @@ const DriverForm = () => {
             console.log("docs key to upload :>> ", key);
             const contentType = "application/pdf";
             const type = "put";
-            if(key !== undefined){
+            if (key !== undefined) {
               const data: any = await getS3SignUrl(key, contentType, type);
 
               if (data?.url) {
@@ -384,14 +385,14 @@ const DriverForm = () => {
           const key = finalProfileImage?.key;
           const contentType = "image/png";
           const type = "put";
-          if(key !== undefined){
+          if (key !== undefined) {
             const data: any = await getS3SignUrl(key, contentType, type);
 
             if (data.url) {
               res = await pushProfilePhotoToS3(data.url, finalProfileImage.file);
             }
           }
-        
+
         }
 
         {
@@ -400,14 +401,14 @@ const DriverForm = () => {
             docsKey.push(key);
             const contentType = "application/pdf";
             const type = "put";
-            if(key !== undefined){
+            if (key !== undefined) {
               const data: any = await getS3SignUrl(key, contentType, type);
 
               if (data.url) {
                 res1 = await pushProfilePhotoToS3(data.url, ele.file);
               }
             }
-           
+
           });
         }
 
@@ -548,11 +549,14 @@ const DriverForm = () => {
   React.useEffect(() => {
     if (options && paramData?.vehicleNumber) {
       if (options[options.length - 1]?.value !== paramData.vehicleNumber) {
-        options.push({
+        // options.push({
+        //   value: paramData?.vehicleNumber,
+        //   label: paramData?.vehicleNumber,
+        // });
+        setOptions([...options, {
           value: paramData?.vehicleNumber,
           label: paramData?.vehicleNumber,
-        });
-        setOptions(options);
+        }]);
         allAvailableVehicles.push(paramData);
       }
     }
@@ -566,7 +570,7 @@ const DriverForm = () => {
 
   React.useEffect(() => {
     getAvailableVehicles();
-  }, []);
+  }, [vehicleType]);
 
   return (
     <>
