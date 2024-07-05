5c4ad6940c541e46f8518b73367972ea256a3e45
Navnath Parte <navnath.parte@code-b.dev>
2024-07-05 18:26:16 +0530

    drivers icon

--- src/routes.tsx
+++ src/routes.tsx
@@ -4,8 +4,6 @@ import React from "react";
 import Dashboard from "views/admin/dashboard";
 import Spots from "views/admin/spots";
 import SpotForm from "views/admin/spots/components/SpotForm";
-import Drivers from "views/admin/drivers";
-import Orders from "views/admin/orders";
 import Vehicles from "views/admin/vehicles";
 import Analytics from "views/admin/analytics";
 import General from "views/admin/settings/general";
@@ -21,11 +19,13 @@ import VehicleTypeList from "views/admin/settings/vehicleTypeList";
 import DriverForm from "views/admin/drivers/driverForm";
 import RideView from "views/admin/rides/rideview";
 import VehicleForm from "views/admin/vehicles/vehicleform";
-import CreateOrder from "views/admin/orders/components/createOrder";
-import { MdHome, MdBarChart, MdFastfood, MdOutlineShoppingCart } from "react-icons/md";
+import { MdHome, MdBarChart, MdFastfood, MdOutlineShoppingCart, MdPerson, GiSteeringWheel } from "react-icons/md";
 import { HiLocationMarker } from "react-icons/hi";
 import { BsChatLeftText } from "react-icons/bs";
 import { IoCarSportSharp, IoSettingsOutline } from "react-icons/io5";
+import Riders from "views/admin/riders";
+import Rides from "views/admin/rides";
+import Drivers from "views/admin/drivers";
 
 export interface RoutesType {
   name: string;
@@ -38,7 +38,7 @@ export interface RoutesType {
 
 const authenticatedRoutes: RoutesType[] = [
   {
-    name: "Dashboard",
+    name: "Dashboards",
     layout: "/admin",
     path: "default",
     icon: <MdHome className="h-6 w-6" />,
@@ -55,6 +55,7 @@ const authenticatedRoutes: RoutesType[] = [
     name: "Spot Form",
     layout: "/admin",
     path: "spot/spot-form",
+    // icon: <MdPerson className="h-6 w-6" />,
     component: <SpotForm />,
     secondary: true,
   },
@@ -62,23 +63,31 @@ const authenticatedRoutes: RoutesType[] = [
     name: "Drivers",
     layout: "/admin",
     path: "drivers",
-    icon: <MdOutlineShoppingCart className="h-6 w-6" />,
+    icon: <GiSteeringWheel className="h-6 w-6" />,
     component: <Drivers />,
+    // secondary: true,
   },
   {
-    name: "Orders",
+    name: "Riders",
     layout: "/admin",
-    path: "order",
-    icon: <MdFastfood className="h-6 w-6" />,
-    component: <Orders />,
+    icon: <MdPerson className="h-6 w-6" />,
+    path: "riders",
+    component: <Riders />,
   },
   {
     name: "Vehicles",
     layout: "/admin",
-    path: "vehicles",
     icon: <IoCarSportSharp className="h-6 w-6" />,
+    path: "vehicles",
     component: <Vehicles />,
   },
+  // {
+  //   name: "Rides",
+  //   layout: "/admin",
+  //   icon: <GiMountainRoad className="h-6 w-6" />,
+  //   path: "rides",
+  //   component: <Rides />,
+  // },
   {
     name: "Analytics",
     layout: "/admin",
@@ -90,42 +99,44 @@ const authenticatedRoutes: RoutesType[] = [
     name: "General",
     layout: "/admin",
     path: "settings/general",
-    component: <General />,
+    component: <General/>,
     secondary: true,
   },
   {
-    name: "Privacy Policy",
+    name: "privacyPolicy",
     layout: "/admin",
     path: "settings/privacyPolicy",
-    component: <PrivacyPolicy />,
+    component: <PrivacyPolicy/>,
     secondary: true,
   },
   {
-    name: "Support",
+    name: "support",
     layout: "/admin",
     path: "settings/support",
-    component: <Support />,
+    component: <Support/>,
     secondary: true,
   },
   {
-    name: "Country Code",
+    name: "Settings",
     layout: "/admin",
     path: "settings/countrycode",
-    component: <CountryCode />,
+    component: <CountryCode/>,
     icon: <IoSettingsOutline className="h-6 w-6" />,
     secondary: true,
   },
   {
-    name: "Vehicle Type List",
+    name: "Settings",
     layout: "/admin",
     path: "settings",
+    icon: <IoSettingsOutline className="h-6 w-6" />,
     component: <VehicleTypeList />,
+    // secondary: true,
   },
   {
-    name: "Flows",
+    name: "Settings",
     layout: "/admin",
     path: "settings/flows",
-    component: <Flows />,
+    component: <Flows/>,
     icon: <IoSettingsOutline className="h-6 w-6" />,
     secondary: true,
   },
@@ -136,53 +147,58 @@ const authenticatedRoutes: RoutesType[] = [
     component: <Fare />,
     secondary: true,
   },
+  // {
+  //   name: "VehicleType",
+  //   layout: "/admin",
+  //   path: "settings/vehicle-type",
+  //   component: <VehicleTypeList />,
+  //   secondary: true,
+  // },
   {
-    name: "Vehicle Type",
+    name: "VehicleType",
     layout: "/admin",
     path: "settings/vehicletypeform",
     component: <VehicleType />,
     secondary: true,
   },
   {
-    name: "Vehicle Type ID",
+    name: "VehicleType Id",
     layout: "/admin",
     path: "settings/vehicletypeform/:id",
     component: <VehicleType />,
     secondary: true,
   },
   {
-    name: "Vehicle Form",
+    name: "VehicleForm",
     layout: "/admin",
     path: "vehicles/vehicleform",
     icon: <MdOutlineShoppingCart className="h-6 w-6" />,
     component: <VehicleForm />,
     secondary: true,
   },
+
   {
-    name: "Vehicle Form ID",
+    name: "VehicleForm Id",
     layout: "/admin",
+    icon: <MdBarChart className="h-6 w-6" />,
     path: "vehicles/vehicleform/:id",
     component: <VehicleForm />,
     secondary: true,
   },
+
   {
     name: "Driver Form",
     layout: "/admin",
     path: "drivers/driverform",
+    // icon: <MdPerson className="h-6 w-6" />,
     component: <DriverForm />,
     secondary: true,
   },
   {
-    name: "Create Order",
-    layout: "/admin",
-    path: "order/add",
-    component: <CreateOrder />,
-    secondary: true,
-  },
-  {
-    name: "Driver Form ID",
+    name: "DriverForm Id",
     layout: "/admin",
     path: "drivers/driverform/:id",
+    // icon: <MdPerson className="h-6 w-6" />,
     component: <DriverForm />,
     secondary: true,
   },
@@ -190,32 +206,33 @@ const authenticatedRoutes: RoutesType[] = [
     name: "Ride Details",
     layout: "/admin",
     path: "rides/ridedetails/:id",
+    // icon: <MdPerson className="h-6 w-6" />,
     component: <RideView />,
     secondary: true,
   },
   {
-    name: "Country Code Form",
+    name: "Country-Form",
     layout: "/admin",
     path: "settings/countrycode-form",
-    component: <CountryCodeForm />,
-    icon: <MdOutlineShoppingCart className="h-6 w-6" />,
-    secondary: true,
+    icon: <MdPerson className="h-6 w-6" />,
+    component: <CountryCodeForm/>,
+    secondary:true
   },
   {
-    name: "Flows Form",
+    name: "Flows-Form",
     layout: "/admin",
     path: "settings/flow-form",
-    component: <FlowsForm />,
-    icon: <MdOutlineShoppingCart className="h-6 w-6" />,
-    secondary: true,
+    icon: <MdPerson className="h-6 w-6" />,
+    component: <FlowsForm/>,
+    secondary:true
   },
   {
-    name: "Flows Form ID",
+    name: "Flows-Form ID",
     layout: "/admin",
     path: "settings/flow-form/:id",
-    component: <FlowsForm />,
-    icon: <MdOutlineShoppingCart className="h-6 w-6" />,
-    secondary: true,
+    icon: <MdPerson className="h-6 w-6" />,
+    component: <FlowsForm/>,
+    secondary:true
   },
 ];
 
