cc3cc6ef1daee5ec10adc910f1db6265b1b02d26
Navnath Parte <navnath.parte@code-b.dev>
2024-07-15 21:23:25 +0530

    fix admin panel

--- src/views/admin/settings/Flow/columnTable.jsx
+++ src/views/admin/settings/Flow/columnTable.jsx
@@ -19,13 +19,14 @@ import { FaCaretDown, FaCaretUp } from "react-icons/fa";
 import Navbar from "components/navbar";
 import { toast } from "react-toastify";
 import Loader from "components/loader/loader";
-import { createAppFlowAPI, getFlow, updateAppFlowAPI } from "services/customAPI";
-
-
+import {
+  createAppFlowAPI,
+  getFlow,
+  updateAppFlowAPI,
+} from "services/customAPI";
 
 function ColumnsTable(props) {
-  const { tableData, handleClickForDeleteModal, handleToggleForStatusMOdal, handleUpdate } =
-    props;
+  const { tableData, handleUpdate } = props;
   // console.log("tableData from index.js", tableData)
   const [sorting, setSorting] = React.useState([]);
   const [loading, setLoading] = useState(false);
@@ -36,7 +37,7 @@ function ColumnsTable(props) {
 
   const handleOptionChange = (event) => {
     setSelectedFlowOption(event.target.value);
-    console.log("[][][][][][]]", event.target.value)
+    console.log("[][][][][][]]", event.target.value);
     // createApplicationFlow(event.target.value);
   };
 
@@ -72,19 +73,16 @@ function ColumnsTable(props) {
   const getApplicationFlow = async () => {
     setIsLoading(true);
     try {
-      const res = await getFlow()
-      setAppFLowId(res.data[0]._id)
-      setSelectedFlowOption(res.data[0].applicationFLow)
-      console.log("respones:>>>>", res.data[0]._id)
+      const res = await getFlow();
+      setAppFLowId(res.data?._id);
+      setSelectedFlowOption(res.data?.applicationFLow);
+      console.log("respones:>>>>", res.data?._id);
       setIsLoading(false);
-
     } catch (error) {
-      errorToast(error.response?.data?.message || "Something went wrong");
+      errorToast(error?.response?.data?.message || "Something went wrong");
       setIsLoading(false);
     }
-  }
-
-
+  };
 
   // APi to create Apllication flow for Driver
   const createApplicationFlow = async () => {
@@ -92,23 +90,21 @@ function ColumnsTable(props) {
     try {
       if (AppFlowId) {
         const data = { selectedFlowOption };
-        console.log("qwaszdxfcgvhbjnkm", data, AppFlowId)
-        const res = await updateAppFlowAPI(AppFlowId, data)
-        console.log("respone:>>>>", res)
+        console.log("qwaszdxfcgvhbjnkm", data, AppFlowId);
+        const res = await updateAppFlowAPI(AppFlowId, data);
+        console.log("respone:>>>>", res);
         setIsLoading(false);
       } else {
         const data = { selectedFlowOption };
-        const res = await createAppFlowAPI(data)
-        console.log("respone :>>>>", res)
+        const res = await createAppFlowAPI(data);
+        console.log("respone :>>>>", res);
         setIsLoading(false);
       }
-
     } catch (error) {
       errorToast(error.response?.data?.message || "Something went wrong");
       setIsLoading(false);
     }
-  }
-
+  };
 
   const columns = [
     columnHelper.accessor("breakingPointName", {
@@ -146,7 +142,7 @@ function ColumnsTable(props) {
         </p>
       ),
       cell: (info) => {
-        console.log("info", info.row.original.action.id)
+        console.log("info", info.row.original.action.id);
         return (
           <div className="flex items-center">
             <div className="cursor-pointer">
@@ -162,7 +158,8 @@ function ColumnsTable(props) {
                 onClick={() => handleClickForDeleteModal(info.row.original)}
               />
             </div> */}
-          </div>)
+          </div>
+        );
       },
     }),
   ]; // eslint-disable-next-line
@@ -180,7 +177,7 @@ function ColumnsTable(props) {
   });
 
   useEffect(() => {
-    getApplicationFlow()
+    getApplicationFlow();
     setData([...tableData]);
   }, [tableData]);
 
@@ -194,53 +191,58 @@ function ColumnsTable(props) {
       ) : (
         <>
           <Card extra="w-full mt-4 pb-10 p-4 h-full">
-            {isLoading ? ( 
-            <Loader /> ):
-            (
-            <div className="mb-5">
-              <label
-                htmlFor="flow"
-                className="input-custom-label dark:text-white"
-              >
-                Choose Application Flow
-              </label>
-              <div className="justify-between gap-5  w-full">
-                <label htmlFor="default" className="mr-8 ">
-                  <input
-                    type="radio"
-                    id="default"
-                    name="option"
-                    value="default"
-                    checked={selectedFlowOption === "default"}
-                    onChange={handleOptionChange}
-                  />
-                  <label className="ml-2">Default</label>
+            {isLoading ? (
+              <Loader />
+            ) : (
+              <div className="mb-5">
+                <label
+                  htmlFor="flow"
+                  className="input-custom-label dark:text-white"
+                >
+                  Choose Application Flow
                 </label>
-                <label htmlFor="custom" className="mr-8">
-                  <input
-                    type="radio"
-                    id="custom"
-                    name="option"
-                    value="custom"
-                    checked={selectedFlowOption === "custom"}
-                    onChange={handleOptionChange}
-                  />
-                  <label className="ml-2">Custom</label>
-                </label>
-                <label htmlFor="custom" className="mr-8">
-                  <input
-                    type="radio"
-                    id="petPuja"
-                    name="option"
-                    value="petPuja"
-                    checked={selectedFlowOption === "petPuja"}
-                    onChange={handleOptionChange}
-                  />
-                  <label className="ml-2">PetPuja</label>
-                </label>
-                <button onClick={() => createApplicationFlow()} className="save-button my-2 ms-1 bg-brand-500 dark:bg-brand-400 sm:my-0">Save Flow</button>
+                <div className="w-full justify-between  gap-5">
+                  <label htmlFor="default" className="mr-8 ">
+                    <input
+                      type="radio"
+                      id="default"
+                      name="option"
+                      value="default"
+                      checked={selectedFlowOption === "default"}
+                      onChange={handleOptionChange}
+                    />
+                    <label className="ml-2">Default</label>
+                  </label>
+                  <label htmlFor="custom" className="mr-8">
+                    <input
+                      type="radio"
+                      id="custom"
+                      name="option"
+                      value="custom"
+                      checked={selectedFlowOption === "custom"}
+                      onChange={handleOptionChange}
+                    />
+                    <label className="ml-2">Custom</label>
+                  </label>
+                  <label htmlFor="custom" className="mr-8">
+                    <input
+                      type="radio"
+                      id="petPuja"
+                      name="option"
+                      value="petPuja"
+                      checked={selectedFlowOption === "petPuja"}
+                      onChange={handleOptionChange}
+                    />
+                    <label className="ml-2">PetPuja</label>
+                  </label>
+                  <button
+                    onClick={() => createApplicationFlow()}
+                    className="save-button my-2 ms-1 bg-brand-500 dark:bg-brand-400 sm:my-0"
+                  >
+                    Save Flow
+                  </button>
+                </div>
               </div>
-            </div>
             )}
             <header className="relative flex items-center justify-between">
               <div className="text-xl font-bold text-navy-700 dark:text-white">
@@ -261,7 +263,10 @@ function ColumnsTable(props) {
               <table className="w-full">
                 <thead>
                   {table.getHeaderGroups().map((headerGroup) => (
-                    <tr key={headerGroup.id} className="!border-px !border-gray-400">
+                    <tr
+                      key={headerGroup.id}
+                      className="!border-px !border-gray-400"
+                    >
                       {headerGroup.headers.map((header) => {
                         return (
                           <th
@@ -270,7 +275,7 @@ function ColumnsTable(props) {
                             onClick={header.column.getToggleSortingHandler()}
                             className="cursor-pointer border-b-[1px] border-gray-200 pb-2 pr-4 pt-4 text-start"
                           >
-                            <div className="flex gap-4 text-xs text-gray-200 text-left">
+                            <div className="flex gap-4 text-left text-xs text-gray-200">
                               {flexRender(
                                 header.column.columnDef.header,
                                 header.getContext()
@@ -278,12 +283,21 @@ function ColumnsTable(props) {
                               {
                                 <>
                                   {header.column.getIsSorted() === "asc" ? (
-                                    <FaCaretUp className="mr-[-6] text-gray-600 font-bold" size={20} />
+                                    <FaCaretUp
+                                      className="mr-[-6] font-bold text-gray-600"
+                                      size={20}
+                                    />
                                   ) : header.column.getIsSorted() === "desc" ? (
-                                    <FaCaretDown size={20} className="text-gray-600 font-bold" />
+                                    <FaCaretDown
+                                      size={20}
+                                      className="font-bold text-gray-600"
+                                    />
                                   ) : (
-                                    <div className="flex mr-[-6]">
-                                      <FaCaretDown size={20} className="text-gray-600 font-bold" />
+                                    <div className="mr-[-6] flex">
+                                      <FaCaretDown
+                                        size={20}
+                                        className="font-bold text-gray-600"
+                                      />
                                     </div>
                                   )}
                                 </>
@@ -298,7 +312,10 @@ function ColumnsTable(props) {
                 {tableData.length == 0 ? (
                   <tbody>
                     <tr>
-                      <td colSpan={columns.length} style={{ textAlign: "center" }}>
+                      <td
+                        colSpan={columns.length}
+                        style={{ textAlign: "center" }}
+                      >
                         <h2 className="m-4" style={{ fontSize: "30px" }}>
                           No Results!
                         </h2>
@@ -343,4 +360,3 @@ function ColumnsTable(props) {
 
 export default ColumnsTable;
 const columnHelper = createColumnHelper();
-
