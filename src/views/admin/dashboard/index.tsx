d17d0051450d7d7ae9710e8e25fdca451b57ef89
Deepak Asati <asatideepak601@gmail.com>
2024-08-01 12:20:39 +0530

    Live location on dashboard isuue fixed

--- src/views/admin/dashboard/index.tsx
+++ src/views/admin/dashboard/index.tsx
@@ -365,8 +365,8 @@ const Dashboard = () => {
                     allOnlineDrivers.length > 0 &&
                     allOnlineDrivers.map((driverId) => {
                       const position = {
-                        lng: driverId.liveLocation && driverId.liveLocation[0],
-                        lat : driverId.liveLocation && driverId.liveLocation[1],
+                        lng: driverId.liveLocation && driverId.liveLocation[1],
+                        lat : driverId.liveLocation && driverId.liveLocation[0],
                       };
                       return (
                         <Marker
--- src/views/login/login.tsx
+++ src/views/login/login.tsx
@@ -155,7 +155,7 @@ export default function SignIn() {
                 <button className="linear mt-2 w-full rounded-xl bg-brand-500 py-[12px] text-base font-medium text-white transition duration-200 hover:bg-brand-600 active:bg-brand-700 dark:bg-brand-400 dark:text-white dark:hover:bg-brand-300 dark:active:bg-brand-200">
                   Log In
                 </button>
-                <div className="mt-4">
+                {/* <div className="mt-4">
                   <span className=" text-sm font-medium text-navy-700 dark:text-gray-600">
                     Not registered yet?
                   </span>
